21aa5d912f01fd73d24ffdcab82f5ba1a0a94bbe
Johan Kinnander <johan@turnpikegroup.com>
2018-01-13 09:47:57 +0100

    Add explanatory comment to empty implementation
    All production code that is subjected to the demonstrated test cases is
    in the 'runme' module. Any others are just there as placeholders to
    avoid 'AttributeError' when mocking.

--- static_class.py
+++ static_class.py
@@ -1,3 +1,6 @@
+# This class does nothing but 'pass' becuase it's implementation is of no relevance
+# to the demonstrated test cases. All production code that is subjected to test
+# is in the 'runme' module.
 class StaticClass():
 
     def staticMethod():
